c7c6e293dbdd36abfb6f0e503122397bb893b458
Seth <seth.chapa@gmail.com>
2025-01-17 12:30:04 -0600

    Made changes from 1/13 Check-in (Layout Changes)

---
+++ index.js
@@ -0,0 +1,20 @@
+function changeImage1() {
+    const heroImage = document.getElementById("mainHero");
+    if (heroImage) {
+        heroImage.style.backgroundImage = "url('img/hero1.jpg')";
+    }
+}
+
+function changeImage2() {
+    const heroImage = document.getElementById("mainHero");
+    if (heroImage) {
+        heroImage.style.backgroundImage = "url('img/hero2.jpg')";
+    }
+}
+
+function changeImage3() {
+    const heroImage = document.getElementById("mainHero");
+    if (heroImage) {
+        heroImage.style.backgroundImage = "url('img/hero3.jpg')";
+    }
+}
